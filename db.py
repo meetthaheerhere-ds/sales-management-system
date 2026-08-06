c58c1ee1d6a76ca24c0598bb95cfc8ebaa20127e
Thaheer <meetthaheerhere@gmail.com>
2026-08-06 17:04:08 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Thaheer@1609",
-        database="sales_management_system"
-    )
